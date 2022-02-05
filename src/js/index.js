2e685e48e5e88cac8e5b0a4059ab62d9d4dbe896
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2022-02-05 13:13:09 -0300

    Final version of the code, I guess

--- src/js/index.js
+++ src/js/index.js
@@ -11,37 +11,85 @@ window.addEventListener('load', () => {
 class CreateTask {
     constructor() {
         this.taskForm = document.querySelector('.task-form')
-        this.event()
+        this.submitEvent()
         this.results = document.querySelector('.results')
+        this.iterator = 0
     }
     
-    event() {
+    submitEvent() {
         this.taskForm.addEventListener('submit', e => {
             e.preventDefault()
             this.handleSubmit()
         })
-    }
+
+    };
+
+    
 
     handleSubmit() {
-        const taskToDo = document.getElementById('task-to-do').value
+        const taskToDo = document.getElementById('task-to-do')
         const errorDivs =   document.querySelectorAll('.error-div')
 
-        if(!taskToDo) {
+        if(!taskToDo.value) {
             this.createError('Cannot insert an empty task')
+            for(let errorDiv of errorDivs) {
+                errorDiv.remove()
+            }
+            return
         }
 
-        for(let errorDiv of errorDivs) {
-            errorDivs[0].remove()
+        if(taskToDo.value) {
+            this.createTask(taskToDo.value)
+            for(let errorDiv of errorDivs) {
+                errorDiv.remove()
+            }
+        }
+
+        this.markDivs()
+    };
+
+    markDivs() {
+        const deleteBtns = this.results.querySelectorAll('.delete')
+        for(let btn of deleteBtns) {
+            btn.addEventListener('click', e => {
+                const parentDiv = btn.parentNode
+                this.deleteDiv(parentDiv)
+            })
         }
     }
+    
+    deleteDiv(div) {
+        div.classList.add('remove')
+ 
+
+        setTimeout(() => {
+            if(div.classList.contains('remove')) {
+                div.remove()
+            }
+        }, 900)
+
+    }
+
+    createTask(task) {
+        const elDiv = document.createElement('div')
+        const elButton = document.createElement('button')
+
+        elDiv.innerHTML = task
+        elButton.innerHTML = 'Done'
+        elButton.classList.add('delete')
+        this.iterator++
+        
+        elDiv.appendChild(elButton)
+        this.results.appendChild(elDiv)
+
+    };
 
     createError(msg) {
         const errDiv = document.createElement('div')
         errDiv.classList.add('error-div')
         errDiv.innerHTML = msg
         this.results.appendChild(errDiv)
-    }
-
+    };
 }
 
 const createTasks = new CreateTask
