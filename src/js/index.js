769063a2ac301c11c0144f2cb5c83513666778af
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2022-02-05 07:03:33 -0300

    Restarted the project due some errors and bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,47 @@
-import styles from '../css/styles.scss' 
+import styles from '../css/styles.scss'
+
+const headerH1 = document.getElementById('header-h1')
+window.addEventListener('load', () => {
+    headerH1.animate([
+        { opacity: 0 },
+        { opacity: 1 }
+    ], { duration: 1000, fill: 'forwards', easing: 'ease-in' })
+})
+
+class CreateTask {
+    constructor() {
+        this.taskForm = document.querySelector('.task-form')
+        this.event()
+        this.results = document.querySelector('.results')
+    }
+    
+    event() {
+        this.taskForm.addEventListener('submit', e => {
+            e.preventDefault()
+            this.handleSubmit()
+        })
+    }
+
+    handleSubmit() {
+        const taskToDo = document.getElementById('task-to-do').value
+        const errorDivs =   document.querySelectorAll('.error-div')
+
+        if(!taskToDo) {
+            this.createError('Cannot insert an empty task')
+        }
+
+        for(let errorDiv of errorDivs) {
+            errorDivs[0].remove()
+        }
+    }
+
+    createError(msg) {
+        const errDiv = document.createElement('div')
+        errDiv.classList.add('error-div')
+        errDiv.innerHTML = msg
+        this.results.appendChild(errDiv)
+    }
+
+}
+
+const createTasks = new CreateTask
